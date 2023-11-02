a462d09bdb0e8045d13daaa72bd54b0c99db90d0
Silas John <sejohn@usc.edu>
2023-11-01 20:54:49 -0700

    completed rudimentary analysis

--- analyze_data.py
+++ analyze_data.py
@@ -14,9 +14,18 @@ tier_two_teams = ["T1", "Bilibili Gaming", "KT Rolster", "Weibo Gaming"]
 tier_three_teams = ["NRG"]
 
 # open player_list file for player_info
+output_csv_file_name_overview = f"2023_Worlds_Swiss/data_analyzed/overview.csv"
+output_file_1 = f"2023_Worlds_Swiss/data_analyzed/top.csv"
+output_file_2 = f"2023_Worlds_Swiss/data_analyzed/jgl.csv"
+output_file_3 = f"2023_Worlds_Swiss/data_analyzed/mid.csv"
+output_file_4 = f"2023_Worlds_Swiss/data_analyzed/adc.csv"
+output_file_5 = f"2023_Worlds_Swiss/data_analyzed/sup.csv"
+output_csv_files = [output_csv_file_name_overview, output_file_1, output_file_2, output_file_3, output_file_4, output_file_5]
+
 player_list = open('player_list.txt', "r")
-output_csv_file_name = f"2023_Worlds_Swiss/data_analyzed/overview.csv"
+
 df_player_tier = []
+df_player_role = []
 df_player_team_names = []
 df_player_names = []
 df_player_costs = []
@@ -32,9 +41,12 @@ for player_info in player_list:
     player_name = player_info.split('|')[2].strip()
     player_id = player_info.split('|')[3].strip()
     player_role = player_info.split('|')[4].strip()
+    player_cost = player_info.split('|')[5].strip()
 
     df_player_team_names.append(team_name)
     df_player_names.append(player_name)
+    df_player_role.append(player_role)
+    df_player_costs.append(player_cost)
 
     player_tier = 0
     if team_name in tier_one_teams:
@@ -76,12 +88,14 @@ for player_info in player_list:
     elif player_role == "Support":
         player_role_sup[tiered_player_name] = standardized_total_pts
 
+    analyzed_player_df = 0
     analyzed_player_df = pandas.DataFrame(
         {
             "player_tier": df_player_tier,
+            "player_role": df_player_role,
             "team_name": df_player_team_names,
             "player_name": df_player_names,
-            "player_cost": "~~~",
+            "player_cost": df_player_costs,
             "games_played": df_player_games_played,
             "player_total_pts": df_player_total_pts,
             "player_standardized_score": df_player_standardized_score
@@ -91,19 +105,36 @@ for player_info in player_list:
     player_num = player_num + 1
 
 # open the GENERAL CSV file
-with open(output_csv_file_name, 'w', newline='') as file:
-    writer = csv.writer(file)
+csv_counter = 0
+for csv_file_name in output_csv_files:
+    with open(csv_file_name, 'w', newline='') as file:
+        writer = csv.writer(file)
 
-    # writer HEADERS to each csv file
-    headers = ["player_tier", "team_name", "player_name", "player_cost", "games_played", "player_total_pts",
-               "player_standardized_score"]
-    writer.writerow(headers)
+        # writer HEADERS to each csv file
+        headers = ["player_tier", "player_role", "team_name", "player_name", "player_cost", "games_played", "player_total_pts",
+                   "player_standardized_score"]
+        writer.writerow(headers)
 
-    # write DATA for each HEADER to csv file
-    for player_row in range(0, player_num):  # iterate number of games played for team
-        row = list(analyzed_player_df.iloc[player_row])
-        print(row)  # DEBUG print statement
-        writer.writerow(row)  # write data to CSV file
+        # write DATA for each HEADER to csv file
+        for player_row in range(0, player_num):  # iterate number of games played for team
+            csv_write = False
+            if analyzed_player_df.iloc[player_row][1] == "Top" and (csv_counter == 0 or csv_counter == 1):
+                csv_write = True
+            elif analyzed_player_df.iloc[player_row][1] == "Jungle" and (csv_counter == 0 or csv_counter == 2):
+                csv_write = True
+            elif analyzed_player_df.iloc[player_row][1] == "Mid" and (csv_counter == 0 or csv_counter == 3):
+                csv_write = True
+            elif analyzed_player_df.iloc[player_row][1] == "ADC" and (csv_counter == 0 or csv_counter == 4):
+                csv_write = True
+            elif analyzed_player_df.iloc[player_row][1] == "Support" and (csv_counter == 0 or csv_counter == 5):
+                csv_write = True
+            else:
+                csv_write = False
+            if csv_write:
+                row = list(analyzed_player_df.iloc[player_row])
+                print(row)  # DEBUG print statement
+                writer.writerow(row)  # write data to CSV file
+    csv_counter = csv_counter + 1
 
 def value_getter(item):
     return item[1]
